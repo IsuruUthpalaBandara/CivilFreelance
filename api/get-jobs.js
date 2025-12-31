30acec5b6283e8e4cfdf101194c5eb7f2f1241f9
Isuru Uthpala Bandara <123003757+IsuruUthpalaBandara@users.noreply.github.com>
2025-12-31 13:17:56 +0530

    Add API handler for fetching verified jobs

---
+++ api/get-jobs.js
@@ -0,0 +1,14 @@
+module.exports = async function handler(req, res) {
+  const response = await fetch(
+    `${process.env.SUPABASE_URL}/rest/v1/jobs?verified=eq.true&order=created_at.desc`,
+    {
+      headers: {
+        apikey: process.env.SUPABASE_KEY,
+        Authorization: `Bearer ${process.env.SUPABASE_KEY}`
+      }
+    }
+  );
+
+  const jobs = await response.json();
+  res.status(200).json(jobs);
+};
